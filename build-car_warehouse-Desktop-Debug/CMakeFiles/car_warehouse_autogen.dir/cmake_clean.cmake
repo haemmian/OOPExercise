07e4312b94c3e47914e995c53f2679bca7899802
haemmian <117271459+haemmian@users.noreply.github.com>
2022-11-03 15:45:55 +0100

    Add files via upload

---
+++ build-car_warehouse-Desktop-Debug/CMakeFiles/car_warehouse_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_warehouse_autogen"
+  "car_warehouse_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/car_warehouse_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
